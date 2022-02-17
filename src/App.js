c26b2bf125b059462fc80455b01628430ecc7395
pedRoot <pedrotorres@outlook.com>
2022-02-17 15:54:24 -0400

    Fixture: Update app.js

--- src/App.js
+++ src/App.js
@@ -61,7 +61,7 @@ function App() {
               className="form-control"
               ref={post_title}
               placeholder="example@email.com"
-              // data-testid="email"
+              data-testid="email"
             />
           </div>
           <div className="form-group">
